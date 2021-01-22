466e79f4d8a2a6591d15128efbbb3d412f07fc7c
ldandoy <ldandoy@overconsulting.net>
2021-01-22 16:16:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World !");
+
+let msg = "Bonjour le monde !"
+
+console.log(msg);
+
+let a = 3;
+let b = 2;
+c = a + b;
+
+console.log(c);
+
+let fruits = ['Banane', "Pomme"];
+
+console.log(fruits);
